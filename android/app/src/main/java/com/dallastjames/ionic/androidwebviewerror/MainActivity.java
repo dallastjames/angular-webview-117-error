76e24370cf99a83ffab4b5b37522a83d27f5d199
Dallas James <dallastjames@gmail.com>
2023-10-03 13:52:13 -0500

    before ionic

---
+++ android/app/src/main/java/com/dallastjames/ionic/androidwebviewerror/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dallastjames.ionic.androidwebviewerror;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dallastjames.ionic.androidwebviewerror',
+  appName: 'angular-webview-error',
+  webDir: 'dist/angular-webview-error',
+  server: {
+    androidScheme: 'customscheme',
+  },
+};
+
+export default config;
